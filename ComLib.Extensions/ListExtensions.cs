15cfcdb3a0ebd92a16cc114f835d44d6ec3e9fa2
Lucas <lzinga@users.noreply.github.com>
2016-02-16 11:43:56 -0800

    Added DistinctBy

--- ComLib.Extensions/ListExtensions.cs
+++ ComLib.Extensions/ListExtensions.cs
@@ -11,5 +11,20 @@ namespace ComLib.Extension
         {
             return string.Format("{0} {{ {1} }}", collection.GetType().Name, string.Join(string.Format(" {0} ", seperator), collection));
         }
+        
+        /// <summary>
+        /// Gets items from a list based on the distinct property.
+        /// </summary>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey> (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
