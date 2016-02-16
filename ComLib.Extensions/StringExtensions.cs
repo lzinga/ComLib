006eebb7f8e0006944013c1203afccd1a7399761
Lucas <lzinga@users.noreply.github.com>
2016-02-16 11:48:10 -0800

    Added contains and TryReplace

--- ComLib.Extensions/StringExtensions.cs
+++ ComLib.Extensions/StringExtensions.cs
@@ -11,5 +11,33 @@ namespace ComLib.Extension
         {
             return string.Format(format, args);
         }
+        
+        /// <summary>
+        /// Checks if the string contains another string.
+        /// </summary>
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+        
+        /// <summary>
+        /// Replaces substring inside string where matched. Will return original if no match found.
+        /// </summary>
+        public static string TryReplace(this string source, string oldString, string newString, StringComparison comp)
+        {
+            int index = source.IndexOf(oldString, comp);
+            bool matchFound = index >= 0;
+
+            if (matchFound)
+            {
+                // Remove the old text.
+                source = source.Remove(index, oldString.Length);
+
+                // Add the new text.
+                source = source.Insert(index, newString);
+            }
+
+            return source;
+        }
     }
 }
